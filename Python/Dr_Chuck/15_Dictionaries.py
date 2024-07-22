f872cab7ebeed42ce94ac7fb3a90946fdca7dcc1
jesse <jesselau24@gmail.com>
2024-07-22 22:44:44 +0300

    1

---
+++ Python/Dr_Chuck/15_Dictionaries.py
@@ -0,0 +1,27 @@
+# list vs dictionary:
+# list is a collection of values in order
+# a dictionary is a bag of values that has labels
+
+# creat an empty dictionary:
+pack = dict()
+
+# put things in it
+pack['money'] = 1200
+pack['water'] = 3
+pack['cat'] = 4
+pack['cyber deck'] = 1
+pack['battery'] = 6
+pack['gun'] = 0
+
+print(pack)
+
+# update dictionary:
+pack['money'] += 2000
+
+# now you see 'money' is 3200:
+print(pack)
+print(pack['money'])
+
+
+
+
