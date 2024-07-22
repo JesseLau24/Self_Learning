e03803a9bf6b88f33c03fcbff54533f316a14758
jesse <jesselau24@gmail.com>
2024-07-22 23:26:23 +0300

    1

--- Python/Dr_Chuck/15_Dictionaries.py
+++ Python/Dr_Chuck/15_Dictionaries.py
@@ -22,6 +22,45 @@ pack['money'] += 2000
 print(pack)
 print(pack['money'])
 
+# make a full dictionary
+character = {'Alestair': 1, 'Morrigan': 1, 'Zevran': 0, 'Liliana': 1}
+print(character)
+
+# Exercise: count frequency
+letters = ['a', 'b', 'c', 'a', 'c', 'd', 'a', 'd', 'a', 'b', 'c', 
+         'a', 'c', 'd','a', 'c', 'd', 'a', 'd', 'a', 'd', 'a', 'b']
+
+freq = dict()
+
+for i in letters:
+    if i not in freq:
+        freq[i] = 1
+    else:
+        freq[i] += 1
+
+print(freq)
+
+# get() method
+freq_a = freq.get('a', 0)
+# it checks if 'a' exist in dict freq
+# if exits, returns the frequency, else, returns 0
+print('a', freq_a)
+
+freq_e = freq.get('e', 0)
+print('e', freq_e)
+
+# so the previous exercise could be rewritten:
+letters = ['a', 'b', 'c', 'a', 'c', 'd', 'a', 'd', 'a', 'b', 'c', 
+         'a', 'c', 'd','a', 'c', 'd', 'a', 'd', 'a', 'd', 'a', 'b']
+
+freq = dict()
+
+for i in letters:
+    freq[i] = freq.get(i, 0) + 1
+
+print(freq)
+
+
 
 
 
