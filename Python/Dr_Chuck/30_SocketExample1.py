774d90fdd8e9136800f977b8517d711b9dbbd73d
jesselau24 <jesselau24@gmail.com>
2024-07-25 18:08:19 +0300

    1

--- Python/Dr_Chuck/30_SocketExample1.py
+++ Python/Dr_Chuck/30_SocketExample1.py
@@ -7,7 +7,7 @@ cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
 mysocket.send(cmd)
 
 while True:
-    data = mysocket.recv(512)
+    data = mysocket.recv(1024) # here you can adjust the number
     if (len(data) < 1):
         break
     print(data.decode())
