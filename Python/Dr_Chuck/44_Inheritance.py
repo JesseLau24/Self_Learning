86cba4e552af70f4a9e550cafe13ff0a49154686
jesselau24 <jesselau24@gmail.com>
2024-07-30 10:49:54 +0300

    1

--- Python/Dr_Chuck/44_Inheritance.py
+++ Python/Dr_Chuck/44_Inheritance.py
@@ -10,6 +10,11 @@ class PartyAnimal:
 
 class FootballFan(PartyAnimal):
     points = 0
+    def __init__(self, nam):
+        super().__init__(nam)  
+        # Call the __init__ method of the parent class
+        # could be ommited, but it's better to keep it
+
     def touchdown(self):
         self.points += 7
         self.party()
@@ -17,9 +22,15 @@ class FootballFan(PartyAnimal):
 
 s = PartyAnimal('Sally')
 s.party()
+# for Sally, the party count would be updated and stored each 
+# time .party() is called
+s.party()
 
 j = FootballFan('Jim')
 j.party()
+j.touchdown() 
+# for Jim,too, the touchdown points and party counts would be 
+# updated and stored in memory
 j.touchdown()
 
 
