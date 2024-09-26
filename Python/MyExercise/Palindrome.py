4d07cc2cb8f546756847d657f4fbd3a63c6b7612
jesse <jesselau24@gmail.com>
2024-09-26 11:47:20 +0300

    not yet understood will get back to it later

--- Python/MyExercise/Palindrome.py
+++ Python/MyExercise/Palindrome.py
@@ -8,3 +8,32 @@ Input: s = "babad"
 Output: "bab"
 要求：可以使用中心扩展算法或动态规划。
 '''
+
+def longest_palindrome(s: str) -> str:
+    if not s or len(s) == 0:
+        return ""
+
+    def expand_around_center(left: int, right: int) -> str:
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return s[left + 1:right]
+
+    longest = ""
+    
+    for i in range(len(s)):
+        # 奇数长度的回文，中心是单个字符
+        odd_palindrome = expand_around_center(i, i)
+        # 偶数长度的回文，中心是两个字符之间
+        even_palindrome = expand_around_center(i, i + 1)
+        
+        # 更新最长的回文子串
+        longest = max(longest, odd_palindrome, even_palindrome, key=len)
+
+    return longest
+
+# 示例
+s = input('Input String: ')
+print(longest_palindrome(s))
+
+
