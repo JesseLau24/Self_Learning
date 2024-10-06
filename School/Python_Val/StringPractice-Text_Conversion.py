0423b3a3d8a522e6c612654444e541d0a44acd80
JesseWin <jesselau24@gmail.com>
2024-10-06 13:40:03 +0300

    Practice 3 would be done on RBP

---
+++ School/Python_Val/StringPractice-Text_Conversion.py
@@ -0,0 +1,19 @@
+'''
+3. Write a program for text conversion
+
+Save user input
+
+Prints the entered text without changes
+
+Exception: if the words in the input are not .... bad, 
+then the output is not ... the bad section must be changed to is good
+
+The weather is not bad -> The weather is good
+
+Car is not new -> Car is not new
+
+This cottage cheese is not so bad -> This cottage cheese is good
+
+Find (or index, or even rfind) will probably come in handy, as may an in operator. 
+Also slice syntax will be useful.
+'''
