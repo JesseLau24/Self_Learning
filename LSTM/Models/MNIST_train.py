65377de1969fa7f98f5fc528c72efbca7c6a3b76
jesse <jesselau24@gmail.com>
2024-10-08 00:03:45 +0300

    做了删减，需要更多测试图片

--- LSTM/Models/MNIST_train.py
+++ LSTM/Models/MNIST_train.py
@@ -59,4 +59,4 @@ end_time = time.time()
 print(f"Elapsed time: {end_time - start_time:.2f} seconds")
 
 # Save Model to Directory
-model.save(r'C:\Users\jesse\Projects\Self_Learning\LSTM\Models\mnist_digit_recognizer.h5')
+model.save('/home/jesse/Projects/Self_Learning/LSTM/Models/mnist_digit_recognizer.keras')
