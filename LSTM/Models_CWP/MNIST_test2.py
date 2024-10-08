58420fb49b5d247a575883823690a12aadd68540
JesseWin <jesselau24@gmail.com>
2024-10-08 21:06:12 +0300

    Seperated the models training on windows and models training on Raspberry Pi

---
+++ LSTM/Models_CWP/MNIST_test2.py
@@ -0,0 +1,24 @@
+from tensorflow.keras.models import load_model # type: ignore
+from PIL import Image
+import numpy as np
+
+# Load Model
+model = load_model('/home/jesse/Projects/Self_Learning/LSTM/Models/t2_mnist_digit_recognizer.keras')
+
+# Load Pic and Convert to Grey
+image = Image.open('/home/jesse/Projects/Self_Learning/LSTM/Pics/6.png').convert('L')
+
+# Resize image (28x28) and choose resampling filter (NEAREST)
+image = image.resize((28, 28), Image.NEAREST)
+
+# Convert to Numpy Array and Normalize to [0, 1]
+image = np.array(image).astype('float32') / 255
+
+# Reshape to (1, 784)
+image = image.reshape(1, 28*28)
+
+# Make prediction
+prediction = model.predict(image)
+predicted_digit = np.argmax(prediction)
+
+print(f'The model predicts this digit is {predicted_digit}.')
