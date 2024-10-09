756b03f91ff9d9a16b0c05bb17bed5c120a8975f
jesse_pc <jesselau24@gmail.com>
2024-10-09 14:37:45 +0300

    Correctly recognized 2, 3, 5, 7, 8, good break through. I'll conder adjusting dropout layer's rate, try batch normalization and perhaps implement early stopping

--- LSTM/Models_CNN_CWP/MNIST_CNN_test.py
+++ LSTM/Models_CNN_CWP/MNIST_CNN_test.py
@@ -3,10 +3,10 @@ from PIL import Image
 import numpy as np
 
 # Load Model
-model = load_model('/home/jesse/Projects/Self_Learning/LSTM/Models_CNN_CWP/t_2cwp_cnn_mnist_digit_recognizer.keras')
+model = load_model('/home/jesse/Projects/Self_Learning/LSTM/Models_CNN_CWP/cwp_cnn_mnist.keras')
 
 # Load Pic and Convert to Grey
-image = Image.open('/home/jesse/Projects/Self_Learning/LSTM/Pics/1.png').convert('L')
+image = Image.open('/home/jesse/Projects/Self_Learning/LSTM/Pics/9.png').convert('L')
 # Resize image (28x28) and choose resampling filter (NEAREST)
 image = image.resize((28, 28), Image.NEAREST)
 
